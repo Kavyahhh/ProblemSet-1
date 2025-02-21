1ecb45f9254154561c3de6423ceb8cdab9d8066b
KavyaRavichandran <101705125+Kavyahhh@users.noreply.github.com>
2025-02-21 14:31:53 +0530

    Update Problem1.java
    Prob 1 : Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.
    
    
    Input:  2 Name y
    
    Expected Output:
    
    2
    
    Name
    
    y

--- Problem1.java
+++ Problem1.java
@@ -1,11 +1,3 @@
-//Prob 1 : Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.
-Input:  2 Name y
-Expected Output:
-2
-Name
-y
-
-CODE : 
 import java.util.Scanner;
 class Main{
     public static void main(String[] args){
